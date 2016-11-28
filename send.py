4940d986737b6b3715fc7485aa979dec4afe9265
Boudy <boudy@skynet>
2016-11-28 20:37:24 +0100

    de plus en plus de fonctions send

--- send.py
+++ send.py
@@ -28,4 +28,21 @@ def send_nr (sock,neigh):
     sock.sendto(make_entete(2) + make_nr() , (neigh.port,neigh.IP))
     
 
+def make_neighbours (neigh_list):
+    l = len(neigh_list)
+    length = 26 * l
+    tlv = bytes([4,length])
+    for k in range (l):
+        neigh = neigh_list[k]
+        tlv := tlv + neigh.Id + neigh.IP + neigh.port
+    return(tlv)
+def send_neighbours (sock,neigh,neigh_list):
+    sock.sendto(make_entete(2+26*len(neigh_list)) + make_neighbours(neigh_list) , (neigh.port,neighIP))
+
+
+
+
+
+
+
 
