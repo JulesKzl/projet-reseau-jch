fafeb0569c062cadfdfc87aa0282f7842171835f
JulesKozolinsky <jules.kozolinsky@ens-cachan.fr>
2016-12-02 15:46:57 +0100

    les bons const

--- const.py
+++ const.py
@@ -1,17 +1,14 @@
 #en IPv4 qui marche sur eduroam
 #IP de Juliusz
-#HOST_JCH = '81.194.27.155'
-HOST_JCH = '138.231.146.117'
+HOST_JCH = '81.194.27.155'
 #toutes les IP
 HOST_SELF= '0.0.0.0'
 #Port de Juliusz
-#PORT_JCH = 1212
-PORT_JCH = 1312
+PORT_JCH = 1212
 #Notre port
 PORT_SELF = 1312
 #Id de Juliusz
-#Id_JCH = bytes.fromhex('68fa34ac85cf9349')
-Id_JCH = bytes.fromhex('1604135e1d0c135e')
+Id_JCH = bytes.fromhex('68fa34ac85cf9349')
 
 #Magic et Version
 ENTETE_UDP = bytes([57,0])
